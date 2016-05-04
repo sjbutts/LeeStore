a61ac0e626535572727f26e68173b52c2e1f6578
Lee McCormick <lzm0010@gmail.com>
2016-05-04 09:20:14 -0400

    added cart view

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,7 +3,7 @@ module CurrentCart
 	def set_cart
 		@cart = Cart.find(session[:cart_id])
 	rescue ActiveRecord::RecordNotFound
-		@cart = Cart.create
+		@cart = Cart.create(user: current_user)
 		session[:cart_id] = @cart.id	
 	end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items, dependent: :destroy
+
+  def subtotal
+  	line_items.to_a.sum { |item| item.total_price }
+  end
 end
